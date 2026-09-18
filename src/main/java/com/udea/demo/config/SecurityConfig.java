3dba0735c527a583f4f3fdd0b80a765997146a47
Isabella <isabellasanchezmejia11@gmail.com>
2026-09-17 21:33:59 -0500

    Resolved conflicts

--- src/main/java/com/udea/demo/config/SecurityConfig.java
+++ src/main/java/com/udea/demo/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                                                 "/api/v1/clientes/**",
                                                 "/api/v1/pedidos/**",
                                                 "/api/v1/rutas/**",
-                .requestMatchers("/api/v1/auth/login",
+                                                "/api/v1/auth/login",
                                                 "/api/v1/auth/refresh",
                                                 "/api/v1/auth/password/**",
                                                 "/api/v1/clientes/registro",
